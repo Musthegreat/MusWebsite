543cec739d1a411ed6eb06621510af6b43dcd61c
Musthegreat <musgamedev@gmail.com>
2025-01-05 22:25:27 -0500

    minor modifications

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,7 +5,7 @@ import pfp from "/src/assets/mus.png";
 function Landing() {
   return(
     <>
-        <h1>welcome to the stupid garbage republic rising website...</h1>
+        <h1>welcome to the <i>nice happy</i> republic rising website...</h1>
         <img id="pfp" src={pfp} width="200" height="200"></img>
         
         <p>oooh look at me, im spinning</p>
