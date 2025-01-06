dc4945699b93d4d27b544c88ccc4df8d42a34536
Musthegreat <musgamedev@gmail.com>
2025-01-05 22:13:27 -0500

    test1

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -5,7 +5,7 @@ function Landing() {
   return(
     <>
         <h1>welcome to the stupid garbage republic rising website...</h1>
-        <img id="pfp" src="/src/assets/mus.png" width="200" height="200"></img>
+        <img id="pfp" src={"./src/assets/mus.png"} width="200" height="200"></img>
         
         <p>oooh look at me, im spinning</p>
     </>
