e7f54ff9814035235ff7dd85813da2d809805c8c
Musthegreat <musgamedev@gmail.com>
2025-01-05 22:15:29 -0500

    that should work

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "/src/styles/landing.css";
+import pfp from "/src/assets/mus.png";
 
 function Landing() {
   return(
     <>
         <h1>welcome to the stupid garbage republic rising website...</h1>
-        <img id="pfp" src={"./src/assets/mus.png"} width="200" height="200"></img>
+        <img id="pfp" src={pfp} width="200" height="200"></img>
         
         <p>oooh look at me, im spinning</p>
     </>
